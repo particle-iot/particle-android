de23eee0560cc852f0c50dff3d5b336d701a9040
Jens Knutson <jens.knutson@gmail.com>
2016-01-19 08:34:18 -0600

    Electron setup support

---
+++ app/src/main/res/raw/electron_setup_variable_injection_js_template.js
@@ -0,0 +1,5 @@
+console.log('init web view to allow setting vars on window object');
+window.mobileClient = 'android';
+window.particleUsername = '%s';
+window.particleAccessToken = '%s';
+console.log('init web view to allow setting vars on window object');
