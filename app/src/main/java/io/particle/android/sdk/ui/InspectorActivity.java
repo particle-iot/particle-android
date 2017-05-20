68a757181f5fceb61285a5760a9f14d012181cde
Julius <julius.skripkauskas@gmail.com>
2017-05-20 15:18:20 +0300

    going back to existing device list screen

--- app/src/main/java/io/particle/android/sdk/ui/InspectorActivity.java
+++ app/src/main/java/io/particle/android/sdk/ui/InspectorActivity.java
@@ -93,14 +93,7 @@ public class InspectorActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            NavUtils.navigateUpTo(this, new Intent(this, DeviceListActivity.class));
+            finish();
             return true;
         } else {
             int actionId = item.getItemId();
--- app/src/main/java/io/particle/android/sdk/ui/TinkerActivity.java
+++ app/src/main/java/io/particle/android/sdk/ui/TinkerActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
@@ -69,14 +68,7 @@ public class TinkerActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            NavUtils.navigateUpTo(this, new Intent(this, DeviceListActivity.class));
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
