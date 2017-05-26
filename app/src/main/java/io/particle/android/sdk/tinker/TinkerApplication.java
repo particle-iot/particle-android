1d6301522cdc74dff1f573c0b6acb7536268f181
Julius <julius.skripkauskas@gmail.com>
2017-05-27 00:37:01 +0300

    adding fabric

--- app/src/main/java/io/particle/android/sdk/tinker/TinkerApplication.java
+++ app/src/main/java/io/particle/android/sdk/tinker/TinkerApplication.java
@@ -2,14 +2,20 @@ package io.particle.android.sdk.tinker;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
 import io.particle.android.sdk.devicesetup.ParticleDeviceSetupLibrary;
 import io.particle.android.sdk.ui.DeviceListActivity;
+import io.particle.sdk.app.BuildConfig;
 
 public class TinkerApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build());
         ParticleDeviceSetupLibrary.init(this, DeviceListActivity.class);
     }
 }
