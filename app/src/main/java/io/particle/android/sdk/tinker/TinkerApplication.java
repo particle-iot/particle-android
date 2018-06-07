b2c0e9b36d444e2dce675b6cb92fe9b4620602f3
Jens Knutson <jens.knutson@gmail.com>
2018-06-07 17:41:57 -0500

    Make debug builds easier for the community
    Apply a simple, albeit unfortunate fix to make debug
    builds Just Work for someone who's just cloned the app
    repo.  This comes with a small but sad extra step for
    the app maintainers.  Better solutions welcome!

--- app/src/main/java/io/particle/android/sdk/tinker/TinkerApplication.java
+++ app/src/main/java/io/particle/android/sdk/tinker/TinkerApplication.java
@@ -2,13 +2,27 @@ package io.particle.android.sdk.tinker;
 
 import android.support.multidex.MultiDexApplication;
 
+import io.particle.android.sdk.ReleaseBuildAppInitializer;
 import io.particle.android.sdk.devicesetup.ParticleDeviceSetupLibrary;
+
 
 public class TinkerApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // HI THERE: doing a release build?  Read the rest of this comment.  (Otherwise, carry on.)
+        //
+        // ReleaseBuildAppInitializer is a per-build type file, intended to avoid initializing
+        // things like analytics when doing debug builds (i.e.: what most people will be doing when
+        // they download the app via GitHub.)
+        //
+        // If you do a release build of an app based on this code, you'll need to manually comment
+        // out this line by hand or otherwise prevent calling the code
+        // inside ReleaseBuildAppInitializer
+        ReleaseBuildAppInitializer.onApplicationCreated(this);
+
         ParticleDeviceSetupLibrary.init(this);
     }
 }
