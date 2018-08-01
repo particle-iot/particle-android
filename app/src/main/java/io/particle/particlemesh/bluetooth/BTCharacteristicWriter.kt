49687c5af0ade692ad8451c9e3f737ef4739dc6c
Jens Knutson <jens.knutson@gmail.com>
2018-08-01 18:26:54 -0500

    Setting this to 50ms seems to produce fewer write retries

--- app/src/main/java/io/particle/particlemesh/bluetooth/BTCharacteristicWriter.kt
+++ app/src/main/java/io/particle/particlemesh/bluetooth/BTCharacteristicWriter.kt
@@ -20,7 +20,7 @@ import java.util.concurrent.ConcurrentLinkedQueue
 @Volatile
 private var HANDLER_THREAD_ID = 1
 // FIXME: think more carefully about these numbers.  What should they be?
-private const val DELAY_BETWEEN_SENDS_MILLIS = 40
+private const val DELAY_BETWEEN_SENDS_MILLIS = 50
 private const val WRITE_ATTEMPTS_BEFORE_CLOSING_CONNECTION = 10
 
 
