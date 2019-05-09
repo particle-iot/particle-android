5c6542a256c449bd537f2ac1837875a61b36137e
Jens Knutson <jens.knutson@gmail.com>
2019-05-09 18:08:44 -0500

    Hack to get around strange problem where fields would disappear when going to the network ID screen and back

--- meshui/src/main/java/io/particle/mesh/ui/controlpanel/ControlPanelMeshInspectNetworkFragment.kt
+++ meshui/src/main/java/io/particle/mesh/ui/controlpanel/ControlPanelMeshInspectNetworkFragment.kt
@@ -10,6 +10,7 @@ import io.particle.mesh.setup.flow.FlowRunnerUiListener
 import io.particle.mesh.ui.R
 import io.particle.mesh.ui.TitleBarOptions
 import kotlinx.android.synthetic.main.fragment_controlpanel_mesh_network_info.*
+import mu.KotlinLogging
 
 
 class ControlPanelMeshInspectNetworkFragment : BaseControlPanelFragment() {
@@ -18,6 +19,11 @@ class ControlPanelMeshInspectNetworkFragment : BaseControlPanelFragment() {
         R.string.p_controlpanel_mesh_inspect_title,
         showBackButton = true
     )
+
+    private val log = KotlinLogging.logger {}
+
+    private var cacheSet = false
+    private var cachedNetwork: Mesh.NetworkInfo? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,10 +35,23 @@ class ControlPanelMeshInspectNetworkFragment : BaseControlPanelFragment() {
 
     override fun onFragmentReady(activity: FragmentActivity, flowUiListener: FlowRunnerUiListener) {
         super.onFragmentReady(activity, flowUiListener)
-        onNetworkInfoUpdated(flowUiListener.mesh.currentlyJoinedNetwork!!)
+        log.info { "onFragmentReady()" }
+        if (cacheSet == false) {
+            log.info { "Getting network from flow listener config blah" }
+            cachedNetwork = flowUiListener.mesh.currentlyJoinedNetwork!!
+            cacheSet = true
+        }
+        onNetworkInfoUpdated(cachedNetwork!!)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        onNetworkInfoUpdated(cachedNetwork!!)
     }
 
     private fun onNetworkInfoUpdated(networkInfo: Mesh.NetworkInfo) {
+        log.info { "onNetworkInfoUpdated(): $networkInfo" }
+
         p_controlpanel_mesh_inspect_network_name.text = networkInfo.name
         p_controlpanel_mesh_inspect_network_pan_id.text = networkInfo.panId.toString()
         p_controlpanel_mesh_inspect_network_xpan_id.text = networkInfo.extPanId.toString()
