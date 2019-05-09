a8875dbf8b2b50defab742567b83e3a49a8fb5ad
Jens Knutson <jens.knutson@gmail.com>
2019-05-09 18:05:48 -0500

    Don't show pairing success screen unless we're doing first time setup

--- mesh/src/main/java/io/particle/mesh/setup/flow/setupsteps/StepShowTargetPairingSuccessful.kt
+++ mesh/src/main/java/io/particle/mesh/setup/flow/setupsteps/StepShowTargetPairingSuccessful.kt
@@ -1,15 +1,19 @@
 package io.particle.mesh.setup.flow.setupsteps
 
+import io.particle.mesh.setup.flow.FlowIntent
+import io.particle.mesh.setup.flow.FlowUiDelegate
 import io.particle.mesh.setup.flow.MeshSetupStep
 import io.particle.mesh.setup.flow.Scopes
 import io.particle.mesh.setup.flow.context.SetupContexts
-import io.particle.mesh.setup.flow.FlowUiDelegate
 import kotlinx.coroutines.delay
 
 
 class StepShowTargetPairingSuccessful(private val flowUi: FlowUiDelegate) : MeshSetupStep() {
 
     override suspend fun doRunStep(ctxs: SetupContexts, scopes: Scopes) {
+        if (ctxs.flowIntent != FlowIntent.FIRST_TIME_SETUP) {
+            return
+        }
         val deviceName = ctxs.requireTargetXceiver().bleBroadcastName
         val shouldWaitBeforeAdvancingFlow = flowUi.onTargetPairingSuccessful(deviceName)
         if (shouldWaitBeforeAdvancingFlow) {
