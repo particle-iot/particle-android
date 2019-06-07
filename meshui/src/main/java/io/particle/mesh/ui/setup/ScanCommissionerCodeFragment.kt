ce1fec1043d9ae26d49098ef31bee748f11df144
Jens Knutson <jens.knutson@gmail.com>
2019-06-07 18:10:17 -0500

    Fix a crash by moving from onViewCreated() to onFragmentReady()

--- meshui/src/main/java/io/particle/mesh/ui/setup/ScanCommissionerCodeFragment.kt
+++ meshui/src/main/java/io/particle/mesh/ui/setup/ScanCommissionerCodeFragment.kt
@@ -1,10 +1,10 @@
 package io.particle.mesh.ui.setup
 
-import android.os.Bundle
-import android.view.View
+import androidx.fragment.app.FragmentActivity
 import com.squareup.phrase.Phrase
 import io.particle.android.sdk.cloud.ParticleCloudSDK
 import io.particle.mesh.setup.BarcodeData.CompleteBarcodeData
+import io.particle.mesh.setup.flow.FlowRunnerUiListener
 import io.particle.mesh.ui.R
 import kotlinx.android.synthetic.main.fragment_scan_commissioner_code.*
 import mu.KotlinLogging
@@ -16,22 +16,22 @@ class ScanCommissionerCodeFragment :  ScanIntroBaseFragment() {
 
     private val log = KotlinLogging.logger {}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
+    override fun onFragmentReady(activity: FragmentActivity, flowUiListener: FlowRunnerUiListener) {
+        super.onFragmentReady(activity, flowUiListener)
         val productName = getUserFacingTypeName()
 
         textView.text = Phrase.from(view, R.string.p_scancommissionercode_tip_content)
-                .put("product_type", productName)
-                .format()
+            .put("product_type", productName)
+            .format()
 
         setup_header_text.text = Phrase.from(view, R.string.pair_assisting_device_with_your_phone)
-                .put("product_type", productName)
-                .format()
+            .put("product_type", productName)
+            .format()
 
         assistantText.text = Phrase.from(view, R.string.p_pairassistingdevice_subheader_1)
-                .put("product_type", productName)
-                .format()
+            .put("product_type", productName)
+            .format()
     }
 
     override fun onBarcodeUpdated(barcodeData: CompleteBarcodeData?) {
