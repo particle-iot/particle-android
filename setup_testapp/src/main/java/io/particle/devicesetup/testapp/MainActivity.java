654fd36a41ae46310ec2dcbb9067cf96ab81132e
Jens Knutson <jens.knutson@gmail.com>
2019-07-15 16:19:43 -0500

    Fixes and updates for example apps

--- setup_testapp/src/main/java/io/particle/devicesetup/testapp/MainActivity.java
+++ setup_testapp/src/main/java/io/particle/devicesetup/testapp/MainActivity.java
@@ -1,7 +1,9 @@
 package io.particle.devicesetup.testapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
