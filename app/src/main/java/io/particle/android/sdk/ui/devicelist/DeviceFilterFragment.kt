902637275fb5f0cae0aefa59729eb528917c57cc
Jens Knutson <jens.knutson@gmail.com>
2019-08-04 04:30:48 -0500

    Make the buttons do something for now

--- app/src/main/java/io/particle/android/sdk/ui/devicelist/DeviceFilterFragment.kt
+++ app/src/main/java/io/particle/android/sdk/ui/devicelist/DeviceFilterFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.particle.sdk.app.R
+import kotlinx.android.synthetic.main.fragment_filter_fragment.*
 
 
 class DeviceFilterFragment : Fragment() {
@@ -26,6 +27,18 @@ class DeviceFilterFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 //        viewModel = ViewModelProviders.of(this).get(DeviceFilterViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        action_close.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        action_filter_device_list.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
     }
 
 }
