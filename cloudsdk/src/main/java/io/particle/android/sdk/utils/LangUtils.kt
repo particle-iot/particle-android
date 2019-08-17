56dbf719872532ef4fa5287c1871ad979cd71fce
Jens Knutson <jens.knutson@gmail.com>
2019-08-16 21:21:46 -0500

    Introduce handy util object for explicit "no op"s

---
+++ cloudsdk/src/main/java/io/particle/android/sdk/utils/LangUtils.kt
@@ -0,0 +1,5 @@
+package io.particle.android.sdk.utils
+
+// A "no-op" value, for use with, e.g.: intentionally non-exhaustive when() statements,
+// inspired by Python's "pass" statement
+val pass = Unit
