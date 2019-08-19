eafa99e19cc11459738804ac54787c977f66ecd8
Jens Knutson <jens.knutson@gmail.com>
2019-08-19 01:04:41 -0500

    Move Toaster to kotlin

---
+++ cloudsdk/src/main/java/io/particle/android/sdk/utils/Toaster.kt
@@ -0,0 +1,51 @@
+package io.particle.android.sdk.utils
+
+import android.app.Activity
+import android.widget.Toast
+
+import javax.annotation.ParametersAreNonnullByDefault
+
+
+@Deprecated(
+    "Deprecated; will be removed in a future release",
+    replaceWith = ReplaceWith("io.particle.mesh.setup.utils.safeToast()")
+)
+@ParametersAreNonnullByDefault
+object Toaster {
+
+    /**
+     * Shows a toast message for a short time.
+     *
+     *
+     * This is safe to call from background/worker threads.
+     */
+    fun s(activity: Activity, msg: String?) {
+        showToast(activity, msg, Toast.LENGTH_SHORT)
+    }
+
+    /**
+     * Shows a toast message for a longer time than [.s].
+     *
+     *
+     * This is safe to call from background/worker threads.
+     */
+    fun l(activity: Activity, msg: String?) {
+        showToast(activity, msg, Toast.LENGTH_LONG)
+    }
+
+
+    private fun showToast(
+        activity: Activity, msg: String?,
+        length: Int
+    ) {
+        Preconditions.checkNotNull(activity, "Activity must not be null!")
+
+        val toastRunnable = { Toast.makeText(activity, msg, length).show() }
+
+        if (EZ.isThisTheMainThread()) {
+            toastRunnable()
+        } else {
+            EZ.runOnMainThread(toastRunnable)
+        }
+    }
+}
