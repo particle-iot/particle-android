ac7a51ffcff7fad29ef9f45dfe3b09d6ebeba8b5
Jens Knutson <jens.knutson@gmail.com>
2019-09-10 10:36:01 -0500

    Add some documentation to these fields

--- mesh/src/main/java/io/particle/mesh/setup/connection/Constants.kt
+++ mesh/src/main/java/io/particle/mesh/setup/connection/Constants.kt
@@ -5,10 +5,11 @@ import java.util.*
 
 val BT_SETUP_SERVICE_ID =           UUID.fromString("6FA90001-5C4E-48A8-94F4-8030546F36FC")
 val BT_PROTOCOL_VERSION_ID =        UUID.fromString("6FA90002-5C4E-48A8-94F4-8030546F36FC")
+// this is "TX" from the Device OS perspective, labelled as such to match docs elsewhere.
+// from the mobile device perspective, this is the characteristic we will subscribe to
+// (i.e.: read from)
 val BT_SETUP_TX_CHARACTERISTIC_ID = UUID.fromString("6FA90003-5C4E-48A8-94F4-8030546F36FC")
+// this is "RX" from the Device OS perspective
+// from the mobile device perspective, this is the characteristic to write to for sending data
+// to the device
 val BT_SETUP_RX_CHARACTERISTIC_ID = UUID.fromString("6FA90004-5C4E-48A8-94F4-8030546F36FC")
-
-
-val NORDIC_BT_SETUP_SERVICE_ID =           UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e")
-val NORDIC_BT_SETUP_TX_CHARACTERISTIC_ID = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e")
-val NORDIC_BT_SETUP_RX_CHARACTERISTIC_ID = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e")
