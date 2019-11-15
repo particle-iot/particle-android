743059758445907991e4d87cee1eecc1f784e789
Jens Knutson <jens.knutson@gmail.com>
2019-11-14 20:39:10 -0600

    Replace usage of deprecated test runner

--- ecjpake4j/src/androidTest/java/io/particle/ecjpake4j/JPAKEInstrumentedTest.kt
+++ ecjpake4j/src/androidTest/java/io/particle/ecjpake4j/JPAKEInstrumentedTest.kt
@@ -1,6 +1,6 @@
 package io.particle.ecjpake4j
 
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert
 import org.junit.Assert.assertFalse
 import org.junit.Test
